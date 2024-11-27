b869f20fd6253a6a6eb45cdcf0ec9a55c3c8cb6a
Mochamad Farhan Fitrahtur Rachmad <farhanfitrahtur@gmail.com>
2024-11-27 11:01:40 +0700

    feat: add endpoint getTenant custom

--- src/index.ts
+++ src/index.ts
@@ -37,7 +37,7 @@ app.use('/api/*', corsAuth)
 const routePath = '/api'
 app.route(`/`, app.get('/', (c) => {
   checkIp(c)
-  return c.text('Hello from chatgpt service! v1.0.3')
+  return c.text('Hello from chatgpt service! v1.0.4')
 }))
 app.route(`${routePath}`, TenantRoutes)
 app.route(`${routePath}`, AuthRoutes)
