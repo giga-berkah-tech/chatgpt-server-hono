7fab3d7378cfde02d251043da1dea81cd270eac1
Mochamad Farhan Fitrahtur Rachmad <farhanfitrahtur@gmail.com>
2024-12-17 10:20:05 +0700

    feat: test fix

--- src/index.ts
+++ src/index.ts
@@ -37,7 +37,7 @@ app.use('/api/*', corsAuth)
 const routePath = '/api'
 app.route(`/`, app.get('/', (c) => {
   checkIp(c)
-  return c.text('Hello from chatgpt service! v1.0.11')
+  return c.text('Hello from chatgpt service! v1.0.12')
 }))
 app.route(`${routePath}`, TenantRoutes)
 app.route(`${routePath}`, AuthRoutes)
@@ -45,24 +45,24 @@ app.route(`${routePath}`, TenantKeyRoutes)
 app.route(`${routePath}`, DateInDbRoutes)
 
 //Websocket
-const { upgradeWebSocket,websocket } =
-  createBunWebSocket<ServerWebSocket>()
+// const { upgradeWebSocket,websocket } =
+//   createBunWebSocket<ServerWebSocket>()
 
-app.get(
-  '/ws',
-  upgradeWebSocket((c) => {
-    checkIp(c)
-    return {
-      // onMessage(event, ws) {
-      //   console.log(`Message from client: ${event.data}`)
-      //   ws.send('Hello from server1!')
-      // },
-      // onClose: () => {
-      //   console.log('Connection closed')
-      // },
-    }
-  })
-)
+// app.get(
+//   '/ws',
+//   upgradeWebSocket((c) => {
+//     checkIp(c)
+//     return {
+//       // onMessage(event, ws) {
+//       //   console.log(`Message from client: ${event.data}`)
+//       //   ws.send('Hello from server1!')
+//       // },
+//       // onClose: () => {
+//       //   console.log('Connection closed')
+//       // },
+//     }
+//   })
+// )
 
 const server = Bun.serve({
   port: 3001,
