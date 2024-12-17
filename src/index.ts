e39401c30c7ff4016d33cb26a6025ad3d7df45f4
Mochamad Farhan Fitrahtur Rachmad <farhanfitrahtur@gmail.com>
2024-12-17 11:05:27 +0700

    feat: test fix

--- src/index.ts
+++ src/index.ts
@@ -37,7 +37,7 @@ app.use('/api/*', corsAuth)
 const routePath = '/api'
 app.route(`/`, app.get('/', (c) => {
   checkIp(c)
-  return c.text('Hello from chatgpt service! v1.0.13')
+  return c.text('Hello from chatgpt service! v1.0.12')
 }))
 app.route(`${routePath}`, TenantRoutes)
 app.route(`${routePath}`, AuthRoutes)
@@ -48,36 +48,26 @@ app.route(`${routePath}`, DateInDbRoutes)
 const { upgradeWebSocket,websocket } =
   createBunWebSocket<ServerWebSocket>()
 
-// app.get(
-//   '/ws',
-//   upgradeWebSocket((c) => {
-//     checkIp(c)
-//     return {
-//       // onMessage(event, ws) {
-//       //   console.log(`Message from client: ${event.data}`)
-//       //   ws.send('Hello from server1!')
-//       // },
-//       // onClose: () => {
-//       //   console.log('Connection closed')
-//       // },
-//     }
-//   })
-// )
+app.get(
+  '/ws',
+  upgradeWebSocket((c) => {
+    checkIp(c)
+    return {
+      // onMessage(event, ws) {
+      //   console.log(`Message from client: ${event.data}`)
+      //   ws.send('Hello from server1!')
+      // },
+      // onClose: () => {
+      //   console.log('Connection closed')
+      // },
+    }
+  })
+)
 
 const server = Bun.serve({
   port: 3001,
-  fetch(request, server){
-    if (server.upgrade(request)){
-      console.log("UPGRADING")
-        return;
-    }else{
-      console.log("NOT UPGRADING")
-      return
-    }
-},
-  websocket:websocketOptions
-  // websocket:websocketOptions,
-
+  websocket: websocketOptions, 
+  fetch: app.fetch,
 });
 
 checkConnRedis()
